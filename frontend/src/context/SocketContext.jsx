f87128033e30dc3d67777dd3b3ec5bb6c5f608a0
Copeta1 <pecolajrobert1@gmail.com>
2025-11-25 11:21:00 +0100

    final fix?

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -21,6 +21,7 @@ export const SocketContextProvider = ({ children }) => {
       const socket = io(API_BASE_URL, {
         query: {
           userId: authUser._id,
+          withCredentials: true,
         },
       });
 
