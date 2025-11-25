01e3e86cb9601d8cf4c850c0ec521744b4cbbe49
Copeta1 <pecolajrobert1@gmail.com>
2025-11-25 11:41:08 +0100

    do

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app as socketApp, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,14 +18,14 @@ dotenv.config();
 
 const ALLOWED_ORIGIN = process.env.FRONTEND_URL || "http://localhost:3000";
 
-socketApp.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 console.log(`CORS Allowed Origin set to: ${ALLOWED_ORIGIN}`);
 console.log(`Is in Production/Deployment mode: ${!!process.env.FRONTEND_URL}`);
 
 console.log(`NODE_ENV is set to: ${process.env.NODE_ENV}`);
 
-socketApp.use(
+app.use(
   cors({
     origin: ALLOWED_ORIGIN,
     credentials: true,
@@ -35,12 +35,12 @@ socketApp.use(
   })
 );
 
-socketApp.use(express.json());
+app.use(express.json());
 app.use(cookieParser());
 
-socketApp.use("/api/auth", authRoutes);
-socketApp.use("/api/messages", messageRoutes);
-socketApp.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 server.listen(PORT, () => {
   connectToMongoDB();
